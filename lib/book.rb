58d95c8581bf53aa2db0b6e2ec12df6ec875ba07
Louise Iyengar <louise.iyengar@flatironschool.com>
2022-02-20 18:22:33 -0500

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+class Book
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title="And Then There Were None")
+    @title = title
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
 
+# class Dog
+#   attr_reader :name
+#   attr_reader :breed
+#   def initialize(name, breed="Mutt")
+#     @name = name
+#     @breed = breed
+#   end
+# end
